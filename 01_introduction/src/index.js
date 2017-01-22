2c8d81cbbc5c63734114841f102299e1d71c1918
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-22 16:53:59 +0100

    first example

--- 01_introduction/src/index.js
+++ 01_introduction/src/index.js
@@ -1,1 +1,26 @@
-console.log('Daje')
+((scope) => {
+  const element = document.querySelector('.square')
+
+  const keyframes = [
+    {
+      backgroundColor: 'red'
+    },
+    {
+      backgroundColor: 'yellow'
+    }
+  ]
+
+  const timing = {
+    duration: 2000,
+    fill: 'forwards',
+    easing: 'ease-in-out'
+  }
+
+  const effect = new scope.KeyframeEffect(element, keyframes, timing)
+
+  const animation = new scope.Animation(effect, document.timeline)
+
+  scope.play = () => {
+    animation.play()
+  }
+})(window)
