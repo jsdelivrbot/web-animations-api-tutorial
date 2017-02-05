c3e92a1d683b0f29d4b4c5692a8d11da1b643027
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-05 16:18:17 +0100

    simple version

--- 01_introduction/src/index.js
+++ 01_introduction/src/index.js
@@ -1,46 +1,35 @@
-((scope) => {
-  const element = document.querySelector('.square')
+const element = document.querySelector('.square')
 
-  const ANIMATION_DURATION = 2000
+const ANIMATION_DURATION = 2000
 
-  let currentColor = 'red'
-  let animation = null
+let currentColor = 'red'
+let animation = null
 
-  const timing = {
-    duration: ANIMATION_DURATION,
-    fill: 'forwards',
-    easing: 'ease-in-out'
+const timing = {
+  duration: ANIMATION_DURATION,
+  fill: 'forwards',
+  easing: 'ease-in-out'
+}
+
+window.changeColor = color => {
+  const keyframes = [
+    {
+      backgroundColor: currentColor
+    },
+    {
+      backgroundColor: color
+    }
+  ]
+
+  currentColor = color
+
+  const effect = new KeyframeEffect(element, keyframes, timing)
+
+  animation = new Animation(effect, document.timeline)
+
+  animation.onfinish = () => {
+    console.log(`the square is now ${color}`)
   }
 
-  scope.changeColor = (color) => {
-    const keyframes = [
-      {
-        backgroundColor: currentColor
-      },
-      {
-        backgroundColor: color
-      }
-    ]
-
-    currentColor = color
-
-    const effect = new scope.KeyframeEffect(element, keyframes, timing)
-
-    animation = new scope.Animation(effect, document.timeline)
-
-    animation.onfinish = () => {
-      console.log(`the square is now ${color}`)
-    }
-
-    animation.play()
-  }
-
-  const range = document.querySelector('#range')
-
-  range.addEventListener('input', () => {
-    if (animation) {
-      animation.pause()
-      animation.currentTime = ANIMATION_DURATION * range.value / 100
-    }
-  }, false)
-})(window)
+  animation.play()
+}
