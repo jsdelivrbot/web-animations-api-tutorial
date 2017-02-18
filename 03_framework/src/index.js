af0000fe6985c14e18557d2ffd64b5cfec08d9f0
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-18 23:57:38 +0100

    added snackbar

--- 03_framework/src/index.js
+++ 03_framework/src/index.js
@@ -1,10 +1,20 @@
 import progressBarFactory from './progressBar'
+import snackbarFactory from './snackbar'
 
 const progressBar = progressBarFactory(document.querySelector('.progress-bar'))
+const snackbar = snackbarFactory(document.querySelector('.snackbar'))
 
 const progressInput = document.getElementById('progress')
+const messageInput = document.getElementById('message')
 
 window.setProgress = () => {
   progressBar.changeValue(progressInput.value)
 }
 
+snackbar.addHideListener(() => {
+  console.log('snackbar closed')
+})
+
+window.show = () => {
+  snackbar.show(messageInput.value)
+}
