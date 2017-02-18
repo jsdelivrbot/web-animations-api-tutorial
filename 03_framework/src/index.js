16b75f2dc76ae876021961e9bf3b7dd667d43d3a
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-19 00:02:51 +0100

    added tabs

--- 03_framework/src/index.js
+++ 03_framework/src/index.js
@@ -1,11 +1,14 @@
 import progressBarFactory from './progressBar'
 import snackbarFactory from './snackbar'
+import tabsFactory from './tabs'
 
 const progressBar = progressBarFactory(document.querySelector('.progress-bar'))
 const snackbar = snackbarFactory(document.querySelector('.snackbar'))
+const tabs = tabsFactory(document.querySelector('.tabs'))
 
 const progressInput = document.getElementById('progress')
 const messageInput = document.getElementById('message')
+const tabsAnimationTypeInput = document.getElementById('tabs-animation-type')
 
 window.setProgress = () => {
   progressBar.changeValue(progressInput.value)
@@ -18,3 +21,12 @@ snackbar.addHideListener(() => {
 window.show = () => {
   snackbar.show(messageInput.value)
 }
+
+tabs.addChangeListener(index => {
+  snackbar.show(`Tab ${index} is now visible`)
+})
+
+tabsAnimationTypeInput.addEventListener('change', event => {
+  tabs.setAnimationType(event.target.value)
+})
+
